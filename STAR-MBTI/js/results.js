feb7fe06b8aa6ff5c52e52660b7c7d291e41570a
ByeoliKim <65668010+ByeoliKim@users.noreply.github.com>
2022-12-25 18:25:49 +0900

    results 수정

--- STAR-MBTI/js/results.js
+++ STAR-MBTI/js/results.js
@@ -1,25 +1,23 @@
 import { results, mbtis } from './data.js'
 
-// 주소 쿼리스트링에서 mbti 값을 가져오기!
-const mbti = new URLSearchParams(location.search).get('mbti')
-const result = results[mbtis[mbti]]
+// 주소 쿼리스트링에서 mbti 값을 가져오기
+const mbti = new URLSearchParams(location.search).get('mbti');
+const result = results[mbtis[mbti]];
 
-// 결과를 출력할 각 요소를 찾아요!
-const titleEl = document.querySelector('.pTitle')
-const characterEl = document.querySelector('.character')
-const boxEls = document.querySelectorAll('.box')
-const jobEls = document.querySelectorAll('.job')
-const lectureEl = document.querySelector('.lecture')
-const lectureImgEl = document.querySelector('.lecture img')
+// 결과를 출력할 각 요소 찾기
+const titleEl = document.querySelector('.pTitle');
+const mbtiEl = document.querySelector('.mbtiTitle');
+const characterEl = document.querySelector('.character');
+const boxEls = document.querySelectorAll('.box');
+const jobEls = document.querySelectorAll('.job');
 
-// 각 요소에 내용을 채워넣어요!
-titleEl.innerHTML = result.title
-characterEl.src = result.character
-boxEls.forEach(function (boxEl, index) {
-  boxEl.innerHTML = result.results[index]
-})
-jobEls.forEach(function (jobEl, index) {
-  jobEl.innerHTML = result.jobs[index]
-})
-lectureEl.href = result.lectureUrl
-lectureImgEl.src = result.lectureImg
+// 각 요소에 내용 채우기
+titleEl.innerHTML = result.title;
+// mbtiEl.innerHTML = mbti;
+characterEl.src = result.character;
+boxEls.forEach((boxEl, index) => {
+  boxEl.innerHTML = result.results[index];
+});
+jobEls.forEach((jobEl, index) => {
+  jobEl.innerHTML = result.jobs[index];
+});
